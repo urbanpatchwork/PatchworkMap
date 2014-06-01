f77f0c64a73185747c1cc89379e8ce331a61b4ec
James Seppi <james.seppi@gmail.com>
2014-06-01 11:25:34 -0500

    mocking json requests

--- js/main-map.js
+++ js/main-map.js
@@ -1,8 +1,12 @@
+
 (function() {
 
   'use strict';
 
-  var app = angular.module('up', ['ngAnimate']);
+  var CATEGORIES_URL = '/mocks/categories.json';
+  var PROJECTS_URL = '/mocks/projects.geojson';
+
+  // *********************************************************
 
   var map = L.map('map', {
     zoomControl: false
@@ -17,20 +21,101 @@
       'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
   }).addTo(map);
 
-  app.controller('MapCtrl', function ($scope) {
+
+  // *********************************************************
+
+  var app = angular.module('up', ['ngAnimate']);
+
+  // *********************************************************
+
+  app.factory('ProjectService', function ($http, $q) {
+    var service = {};
+
+    service.fetch = function(options) {
+      //options.selectedArea - {point, radius}
+      //options.categoryIds - specific category ids to filter
+      var deferred = $q.defer();
+      $http.get(PROJECTS_URL)
+        .success(function(results) {
+          //TODO: filter based on categoryIds
+          // and selectedArea
+          deferred.resolve(results);
+        });
+
+      return deferred.promise;
+    };
+
+    return service;
+  });
+
+
+  // *********************************************************
+
+  app.factory('CategoryService', function ($http) {
+    var service = {};
+
+    service.fetch = function() {
+      return $http.get(CATEGORIES_URL)
+    };
+
+    return service;
+  });
+
+
+  // *********************************************************
+
+  app.controller('MapCtrl', function ($scope, ProjectService, CategoryService) {
     $scope.showCategories = false;
     $scope.selectedArea = null; //ZIPCode or point-radius
-    $scope.currentProjects = null;
+    $scope.currentProjects = null; //current project features
+    $scope.categories = []; //all valid categories
+    $scope.selectedCategories = []; //only selected categories
 
+    map.invalidateSize(false); //must call because of ng-cloak
+
+    //retrieve all the valid categoies
+    CategoryService.fetch()
+      .success(function (cats) {
+        //default to all set to isSelected
+        _.each(cats, function (c) { c.isSelected = true; });
+
+        //set $scope.categories to the fetched results
+        angular.copy(cats, $scope.categories);
+      });
+
+
+
+    ProjectService.fetch();
+
+
+
+    _.each($scope.categories, function (cat) {
+      cat.isSelected = true;
+    });
 
     $scope.toggleListView = function() {
       console.log('toggle list view');
     };
 
-    $scope.toggleCategories = function() {
-      console.log('toggle categories');
-      $scope.showCategories = !$scope.showCategories;
+    $scope.toggleCategory = function(cat) {
+      cat.isSelected = !cat.isSelected;
     };
+
+
+    $scope.$watch('categories', function(newvals, oldvals) {
+      //skip the first time this watch is triggered
+      if (_.isEmpty(newvals) && _.isEmpty(oldvals)) {
+        return;
+      }
+
+      var selectedCats = _.where($scope.categories, {isSelected: true});
+      angular.copy(selectedCats, $scope.selectedCategories);
+    }, true);
+
+    $scope.$watch('selectedCategories', function() {
+      console.log('selectedCategories', $scope.selectedCategories);
+
+    }, true);
 
   });
 
